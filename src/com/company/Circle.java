7f93a1e437c77c7f5af9fdb9c14a8e3f8f78414a
SolovevaSnastasiya <floflo134@gmail.com0>
2016-11-29 17:21:10 +0400

    класс Круг

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -12,6 +12,16 @@ public class Circle {
         this.r = r;
     }
 
+    public Circle (double x, double y, double r){
+        if (r<=0){
+            throw new RuntimeException("Радиус должен быть положительным");
+        }
+        this.r = r;
+        this.x = x;
+        this.y =y;
+    }
+
+
     public void move(double dx, double dy){
         this.x+=dx;
         this.y+=dy;
@@ -22,5 +32,23 @@ public class Circle {
         return length;
         }
 
+    public void moveTo(double x, double y) {
+        this.x=x;
+        this.y=y;
+    }
+
+    public double diametr(){
+        double d = 2*this.r;
+        return d;
+    }
+
+    public double circleSquare(){
+        double square = this.r*this.r*Math.PI;
+        return square;
+    }
+
+
 
 }
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,13 @@ public class Main {
         System.out.println("Circle:" + c.x + "," + c.y+","+c.r);
         double length=c.circleLength();
         System.out.println("Длина окружности равна:"+ length);
+        Circle c1=new Circle(2,3,5);
+        System.out.println("Circle1:" + c1.x + "," + c1.y+","+c1.r);
+        c1.moveTo(3,12);
+        System.out.println("Circle1:" + c1.x + "," + c1.y+","+c1.r);
+        double d= c1.diametr();
+        System.out.println("Диаметр1:"+ d);
+        double s= c1.circleSquare();
+        System.out.println("Площадь круга1:"+ s);
     }
 }
